5120f0b1d80615c1bffac39b448bf4ab642f7f63
Luan <9001545@riachuelo.com.br>
2022-06-24 20:29:19 -0300

    add form

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-eu-west-2.graphcms.com/v2/cl4ogfawp04q001xs2eclae2j/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+   'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache() // Estratégia de cache para controle de requisições 
 })
